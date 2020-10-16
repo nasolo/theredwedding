3722ff11b171bcdf7b601007179d281ed12b7917
Home and Abroad <momemacpc@Homes-MBP.attlocal.net>
2020-10-15 20:14:45 -0500

    margin top

--- src/pages/contact/component/chatNavigation/index.js
+++ src/pages/contact/component/chatNavigation/index.js
@@ -11,7 +11,7 @@ const ChatNavigation = props => {
     <Container 
         fluid 
         position="relative"
-        margin="auto auto 0"
+        margin="5rem auto 0"
         
     >
         <LinkWrapper>
